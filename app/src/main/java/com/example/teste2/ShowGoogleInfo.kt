16cfb8042aa4e8725d794db4fef2617dfffde566
caiofelix <cvff12345@gmail.com>
2020-03-23 15:23:25 -0300

    Initial commit

---
+++ app/src/main/java/com/example/teste2/ShowGoogleInfo.kt
@@ -0,0 +1,75 @@
+package com.example.teste2
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_show_google_info.*
+
+class ShowGoogleInfo : AppCompatActivity() {
+
+    lateinit var etNome2: TextView
+
+    lateinit var etEmail2: TextView
+
+    lateinit var imgFoto2: ImageView
+
+    lateinit var googleSignInClient: GoogleSignInClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_google_info)
+
+        etNome2 = findViewById(R.id.textoNome2)
+
+        imgFoto2 = findViewById(R.id.imagemtest2)
+
+        etEmail2 = findViewById(R.id.textoEmail2)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .requestProfile()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        val acct = GoogleSignIn.getLastSignedInAccount(this)
+
+        if (acct != null) {
+
+            var nomePessoa: String? = acct.displayName
+            var imgPesosa: Uri? = acct.photoUrl
+            var emailPessoa: String? = acct.email
+
+
+            etNome2.setText(nomePessoa)
+            etEmail2.setText(emailPessoa)
+            Glide.with(this).load(imgPesosa).into(imgFoto2)
+
+            ButSair.setOnClickListener {googleSignInClient.signOut()
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)}
+
+            imageButtonVoltar.setOnClickListener{googleSignInClient.signOut()
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)}
+
+            }
+        }
+
+
+    }
+
+
+
+
+
